0fac4d9e664336c94e90fa2f9025dd94c05e8cd5
collinsjacob127 <collinsjacob127@gmail.com>
2022-07-17 12:06:58 -0700

    Changed calls from std::mem to the .take() function from Options type

--- src/second.rs
+++ src/second.rs
@@ -1,8 +1,10 @@
-// Jacob Collins
-// Second.rs
-// Singly-Linked Stack
-// July 17, 2022
-use std::mem;
+/*
+Jacob Collins
+Second.rs
+Singly-Linked Stack
+Based on tutorial from: https://rust-unofficial.github.io/too-many-lists/index.html
+July 17, 2022
+*/
 
 pub struct List {
     head: Link,
@@ -23,14 +25,14 @@ impl List {
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem: elem,
-            next: mem::replace(&mut self.head, None),
+            next: self.head.take(),
         });
 
         self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match mem::replace(&mut self.head, None) {
+        match self.head.take() {
             None => None,
             Some(node) => {
                 self.head = node.next;
@@ -42,10 +44,10 @@ impl List {
 
 impl Drop for List {
     fn drop(&mut self) {
-        let mut cur_link = mem::replace(&mut self.head, None);
+        let mut cur_link = self.head.take();
         // `while let` == "do this thing until this pattern doesn't match"
         while let Some(mut boxed_node) = cur_link {
-            cur_link = mem::replace(&mut boxed_node.next, None);
+            cur_link = boxed_node.next.take();
             // boxed_node goes out of scope and gets dropped here;
             // but its Node's `next` field has been set to Link::Empty
             // so no unbounded recursion occurs.
