d16ee915f6ece4b96b536b0a1725271f4766c9d8
collinsjacob127 <collinsjacob127@gmail.com>
2022-07-27 18:29:54 -0700

    Constructors for List and Node
    Added tail reference to List struct, and prev reference to Node struct.

--- doubly_linked_deque.rs
+++ doubly_linked_deque.rs
@@ -6,78 +6,35 @@ Based on tutorial from: https://rust-unofficial.github.io/too-many-lists/index.h
 Used to learn about interior mutability, and why doubly linked deques are bad.
 July 27, 2022
 */
-use std::rc::Rc;
 // use std::sync::Arc; <- Rc but with Atomics implemented, making it safe for multithreading
+use std::rc::Rc;
+use std::cell::RefCell;
 pub struct List<T> {
     head: Link<T>,
+    tail: Link<T>,
 }
 
-type Link<T> = Option<Rc<Node<T>>>;
+type Link<T> = Option<Rc<RefCell<Node<T>>>>;
 
 struct Node<T> {
     elem: T,
     next: Link<T>,
+    prev: Link<T>
+}
+
+impl<T> Node<T> {
+    pub fn new(elem: T) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Node {
+            elem: elem,
+            next: None,
+            prev: None,
+        }))
+    }
 }
 
 impl<T> List<T> {
     pub fn new() -> Self {
-        List { head: None }
-    }
-
-    pub fn prepend(&self, elem: T) -> List<T> {
-        List { head: Some(Rc::new(Node {
-            elem: elem,
-            next: self.head.clone(),
-        }))}
-    }
-
-    pub fn tail(&self) -> List<T> {
-        List { head: self.head.as_ref().and_then(|node| node.next.clone()) }
-    }
-
-    pub fn head(&self) -> Option<&T> {
-        self.head.as_ref().map(|node| &node.elem)
-    }
-}
-
-pub struct Iter<'a, T> {
-    next: Option<&'a Node<T>>,
-}
-
-impl<T> List<T> {
-    pub fn iter(&self) -> Iter<'_, T> {
-        Iter { next: self.head.as_deref() }
-    }
-}
-
-impl<'a, T> Iterator for Iter<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next.map(|node| {
-            // Other option for doing the below task:
-            // self.next = node.next.as_ref().map::<&Node<T>, _>(|node| &node);
-            // Turbofish!!! ::<>
-            self.next = node.next.as_deref();
-            &node.elem
-        })
-    }
-}
-
-impl<T> Drop for List<T> {
-    fn drop(&mut self) {
-        let mut head = self.head.take();
-        // `while let` == "do this thing until this pattern doesn't match"
-        while let Some(node) = head {
-            if let Ok(mut node) = Rc::try_unwrap(node) {
-                head = node.next.take();
-            } else {
-                break;
-            }
-            // boxed_node goes out of scope and gets dropped here;
-            // but its Node's `next` field has been set to Link::Empty
-            // so no unbounded recursion occurs.
-        }
+        List { head: None, tail: None }
     }
 }
 
@@ -90,7 +47,7 @@ mod test {
 
         // Check empty list behaves right
         assert_eq!(list.head(), None);
-        
+
         let list = list.prepend(1).prepend(2).prepend(3);
         assert_eq!(list.head(), Some(&3));
 
@@ -109,7 +66,7 @@ mod test {
         let list = List::new().prepend(1).prepend(2).prepend(3);
 
         let mut iter = list.iter();
-        
+
         assert_eq!(iter.next(), Some(&3));        
         assert_eq!(iter.next(), Some(&2));        
         assert_eq!(iter.next(), Some(&1));        
