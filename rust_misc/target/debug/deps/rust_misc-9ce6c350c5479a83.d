5a05fb612bfcd5e5f0ef0e0fc7b977784bcd4596
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-08 00:07:05 -0700

    adding rust_misc

---
+++ rust_misc/target/debug/deps/rust_misc-9ce6c350c5479a83.d
@@ -0,0 +1,7 @@
+/home/zato-1/Documents/Learning Rust/rust_misc/target/debug/deps/rust_misc-9ce6c350c5479a83.rmeta: src/main.rs
+
+/home/zato-1/Documents/Learning Rust/rust_misc/target/debug/deps/rust_misc-9ce6c350c5479a83.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
---
+++ rust_misc/target/debug/deps/rust_misc-bb7497666a44a09d.d
@@ -0,0 +1,7 @@
+/home/zato-1/Documents/Learning Rust/rust_misc/target/debug/deps/rust_misc-bb7497666a44a09d.rmeta: src/main.rs
+
+/home/zato-1/Documents/Learning Rust/rust_misc/target/debug/deps/rust_misc-bb7497666a44a09d.d: src/main.rs
+
+src/main.rs:
+
+# env-dep:CLIPPY_ARGS=
