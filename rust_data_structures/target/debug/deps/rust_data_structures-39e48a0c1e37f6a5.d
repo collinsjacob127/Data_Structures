831d0314c00550a4043634f829caadd0f132efcb
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-08 00:39:11 -0700

    Rebased guessing game, started notes for rust_misc

---
+++ rust_data_structures/target/debug/deps/rust_data_structures-39e48a0c1e37f6a5.d
@@ -0,0 +1,12 @@
+/home/zato-1/Documents/Learning Rust/rust_data_structures/target/debug/deps/rust_data_structures-39e48a0c1e37f6a5.rmeta: src/lib.rs src/basic_linked_list.rs src/singly_linked_stack.rs src/persistent_singly_linked_stack.rs src/doubly_linked_deque.rs src/silly_double_singly_linked_list.rs
+
+/home/zato-1/Documents/Learning Rust/rust_data_structures/target/debug/deps/rust_data_structures-39e48a0c1e37f6a5.d: src/lib.rs src/basic_linked_list.rs src/singly_linked_stack.rs src/persistent_singly_linked_stack.rs src/doubly_linked_deque.rs src/silly_double_singly_linked_list.rs
+
+src/lib.rs:
+src/basic_linked_list.rs:
+src/singly_linked_stack.rs:
+src/persistent_singly_linked_stack.rs:
+src/doubly_linked_deque.rs:
+src/silly_double_singly_linked_list.rs:
+
+# env-dep:CLIPPY_ARGS=
--- rust_misc/src/main.rs
+++ rust_misc/src/main.rs
@@ -1,3 +1,49 @@
+#[ignore = "dead_code"]
 fn main() {
-    println!("Hello, world!");
+    // ------------------------ VARS ------------------------
+    // --- Constants ---
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    // --- Shadowing ---
+    // typically used for immutable vars
+    let x = 5; // <- immutable
+    let x = x + 1;
+
+    // allows us to change types within same variable
+    let spaces = "   ";         // <- type: String()
+    let spaces = spaces.len();  // <- type: usize
+
+    // mutability would actually prevent this
+    // let mut spaces = "   ";
+    // spaces = spaces.len();   // <- COMPILER ERROR
+
+    // -------------------- Data Types ----------------------
+    // if many types are possible for the result, must add a 
+    // type annotation
+    let guess: u32 = "42".parse().expect("Not a number!");
+    
+    // ---- Scalar Types ---
+    // represent a single value
+
+    // Integer
+    // type suffix
+    let num1 = 57u8; // 57 can be multiple types, u8 specifies
+    let num2 = 1000;
+    let num3 = 1_000;
+    assert_eq!(num2, num3);
+    
+    // integer overflow in regular runtime causes panic!
+    // integer overflow in --release gets twos compliment
+
+    // Floating Point
+    let num4 = 2.0;         // f64
+    let num5: f32 = 3.0;    // f32
+
+    // Character 
+    let c = 'z';
+    let z: char = 'ℤ'; // with explicit type annotation
+    let heart_eyed_cat = '😻';
+
+    // --- Compound Types ---
+
 }
