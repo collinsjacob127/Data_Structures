d81322cdfdf47697d47e30617722359d2315e733
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-07 23:47:43 -0700

    Finished guessing game

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -1,3 +1,38 @@
+/* Jacob Collins
+ * Rust guessing game for practicing io and strings
+ * August 7, 2022
+ */
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop { 
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {           // trim() removes
+            Ok(num) => num,                                     // whitespace and \n
+            Err(_) => continue,                                 // from terminal
+        };                                                      // input (when you                 
+                                                                // press enter)                    
+
+        println!("You guessed: {guess}");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Large!"),
+            Ordering::Equal => {
+                println!("Too Perfect!");
+                break; 
+            },
+        }
+    }
 }
