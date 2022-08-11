10b6f3fd24755cc352b115492e4257e0dc43ec63
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-11 15:09:52 -0700

    more enum functionality

--- rust-misc/src/enums.rs
+++ rust-misc/src/enums.rs
@@ -4,4 +4,22 @@
  * August 11, 2022
  */
 
-pub fn enums() {}
+pub fn enums() {
+    // Enums are basically much more flexible structs
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    enum IpAddr {
+        V4(u8, u8, u8, u8),
+        V6(String),
+    }
+
+    let home = IpAddr::V4(127, 0, 0, 1);
+
+    let loopback = IpAddr::V6(String::from("This"));
+}
