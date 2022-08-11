988b084a3d48278ab33c2471dab55caa299b6456
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-11 00:36:10 -0700

    renaming files

--- rust-data-structures/src/lib.rs
+++ rust-data-structures/src/lib.rs
@@ -1,12 +1,12 @@
 #[path = "basic-linked-list.rs"]
 pub mod basic_linked_list;
+#[path = "double-singly-linked-list.rs"]
+pub mod double_singly_linked_list;
 #[path = "doubly-linked-deque.rs"]
 pub mod doubly_linked_deque;
 #[path = "persistent-singly-linked-stack.rs"]
 pub mod persistent_singly_linked_stack;
-#[path = "silly-double-singly-linked-list.rs"]
-pub mod silly_double_singly_linked_list;
 #[path = "singly-linked-stack.rs"]
 pub mod singly_linked_stack;
+// #[path = "ok-unsafe-queue.rs"]
 // pub mod ok_unsafe_queue;
-
