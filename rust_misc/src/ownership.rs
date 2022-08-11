75f88ecb3e87efa6129fc63c8d21aa1e90af83ce
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-11 00:05:34 -0700

    Finished ownership and slices

--- rust_misc/src/ownership.rs
+++ rust_misc/src/ownership.rs
@@ -63,16 +63,21 @@ pub fn ownership() {
     // type of reference, so no ownership
     let s = String::from("hello world!");
 
-    fn first_word(s: &String) -> &str {
+    fn first_word(s: &str) -> &str {
         let bytes = s.as_bytes();
         for (i, &item) in bytes.iter().enumerate() {
             if item == b' ' {
                 return &s[..i];
             }
         }
-        &s[..]
+        s
     }
 
     let word = first_word(&s);
     println!("{}", word);
+
+    // --- Non-String Slices ---
+    let a = [1, 2, 3, 4, 5];
+    let slice = &a[1..3];
+    assert_eq!(slice, &[2, 3])
 }
