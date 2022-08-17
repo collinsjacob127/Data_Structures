7a56d2f23bd5a6b2714bdb9dab93c0f23805b7bd
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-17 02:45:29 -0700

    finished hashes. started practice exercises

--- rust-notes/src/collections.rs
+++ rust-notes/src/collections.rs
@@ -185,3 +185,35 @@ pub fn collections() {
 
     println!("{:?}", &word_counter);
 }
+
+// Given a list of integers, use a vector and return the median (when sorted, the value in the middle position) and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+pub fn median(ints: &Vec<i32>) -> i32 {
+    ints[ints.len() / 2]
+}
+
+pub fn mode(ints: &Vec<i32>) -> i32 {
+    let mut int_counts = HashMap::new();
+
+    for i in ints {
+        let count = int_counts.entry(i).or_insert(0);
+        *count += 1;
+    }
+
+    let mut answer = int_counts.get(&ints[0]).unwrap();
+
+    for i in &int_counts {
+        if i.1 > answer {
+            answer = i.0;
+        }
+    }
+
+    *answer
+}
+
+// Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
+pub fn pig_latin(input: &String) -> String {
+    let chars = input.chars();
+    format!(chars[1..].or_insert(""), "-", chars[0], "ay")
+}
+
+// Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
--- rust-notes/src/main.rs
+++ rust-notes/src/main.rs
@@ -2,7 +2,10 @@
  * main.rs
  * Combines notes etc for output and testing
  */
-use crate::{basic::basic, car::car, collections::collections, enums::enums, ownership::ownership};
+use crate::{
+    basic::basic, car::car, collections::collections, collections::median, collections::mode,
+    enums::enums, ownership::ownership,
+};
 
 pub mod basic;
 pub mod car;
@@ -15,5 +18,8 @@ fn main() {
     // ownership();
     // enums();
     // car();
-    collections();
+    //     collections();
+    let num_list = vec![0, 1, 2, 3, 4, 4, 5];
+    println!("Mode: {}", mode(&num_list));
+    println!("Median: {}", median(&num_list));
 }
