f3e3021b83369de74d76ebca039869ef8a7695cb
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-17 16:22:36 -0700

    starting error handling

---
+++ rust-notes/src/error_handling.rs
@@ -0,0 +1,9 @@
+/* Jacob Collins
+ * error_handling.rs
+ * practices for handling various errors
+ * August 17, 2022
+ */
+
+pub fn error_handling() {
+    ()
+}
--- rust-notes/src/main.rs
+++ rust-notes/src/main.rs
@@ -1,18 +1,19 @@
-use crate::collections::pig_latin;
 /* Jacob Collins
  * main.rs
  * Combines notes etc for output and testing
  */
+
 #[allow(unused_imports)]
 use crate::{
     basic::basic, car::car, collections::collections, collections::median, collections::mode,
-    enums::enums, ownership::ownership,
+    collections::pig_latin, enums::enums, error_handling::error_handling, ownership::ownership,
 };
 
 pub mod basic;
 pub mod car;
 pub mod collections;
 pub mod enums;
+pub mod error_handling;
 pub mod ownership;
 
 fn main() {
@@ -21,8 +22,8 @@ fn main() {
     // enums();
     // car();
     //     collections();
-    let num_list = vec![0, 1, 2, 3, 4, 4, 5];
-    println!("Mode: {}", mode(&num_list));
-    println!("Median: {}", median(&num_list));
-    pig_latin("A very hairy man died today. He was quite thick.");
+    //    let num_list = vec![0, 1, 2, 3, 4, 4, 5];
+    //    println!("Mode: {}", mode(&num_list));
+    //    println!("Median: {}", median(&num_list));
+    //    pig_latin("A very hairy man died today. He was quite thick.");
 }
