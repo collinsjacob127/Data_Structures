cd14d48c8e47ab1bd349123c384cbadbca403d52
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-17 17:39:26 -0700

    file create and error match

--- rust-notes/src/error_handling.rs
+++ rust-notes/src/error_handling.rs
@@ -5,12 +5,21 @@
  */
 
 use std::fs::File;
+use std::io::ErrorKind;
 
 pub fn error_handling() {
     let greeting_file_result = File::open("hello.txt");
 
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
-        Err(err) => panic!("File read failed: {}", err),
+        Err(err) => match err.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Error with file create: {}", e),
+            },
+            other_error => {
+                panic!("Error with file open: {}", other_error);
+            }
+        },
     };
 }
