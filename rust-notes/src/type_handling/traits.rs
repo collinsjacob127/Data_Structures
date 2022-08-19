c513f505dee8301a31ae89c536b2229912ec30d3
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-18 21:45:47 -0700

    implementing trait to struct

--- rust-notes/src/type_handling/traits.rs
+++ rust-notes/src/type_handling/traits.rs
@@ -14,5 +14,30 @@ pub trait Summary {
     fn summarize(&self) -> String;
 
     // There can be multiple necessary methods per trait
-    fn as_string(&self) -> str;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
 }
