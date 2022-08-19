b6803cf265da0686436fc2322ab4133f2bd69dbf
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-18 22:30:19 -0700

    restricting parameter traits

--- rust-notes/src/type_handling/traits.rs
+++ rust-notes/src/type_handling/traits.rs
@@ -1,13 +1,59 @@
-/* Name: Jacob Collins
-* Description:
+/*
+Name: Jacob Collins
+Description:
 Notes on traits, their uses, implementations, etc.
-* Last Edited: August XX 2022
+Last Edited: August XX 2022
 */
+
+#[allow(unused_imports)]
+use core::fmt::Display;
 
 pub fn traits() {
     todo!();
 }
 
+// ------- TRAIT RETURNS -------
+#[allow(dead_code)]
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// ------- TRAIT PARAMETERS -------
+// where -> easy to read trait parameters
+#[allow(dead_code, unused_variables)]
+fn some_function<T, U>(x: T, y: U)
+where
+    T: Summary + Clone,
+    U: Display,
+{
+    todo!()
+}
+
+// &impl Summary limits 'item' to types which implement summary
+// + syntax to specify multiple necessary traits
+#[allow(dead_code)]
+fn notify(item: &(impl Summary + Display)) {
+    println!("Breaking news: {}", item.summarize());
+}
+
+/*
+&impl is syntactic sugar for Trait-Bound:
+    pub fn notify<T: Summary>(item: &T) {
+    println!("Breaking news! {}", item.summarize());
+}
+*/
+
+/*
+forces all parameters of T to be the same type
+pub fn notify<T: Summary + Display>(item1: &T, item2: &T) {...}
+*/
+
+// ------- TRAIT DEFINITIONS & IMPLEMENTATIONS -------
 // Orphan rule:
 // you can't define a non-local trate (e.g. Display) on a non-local type (e.g. Vec)
 pub trait Summary {
@@ -36,14 +82,14 @@ pub struct NewsArticle {
 // Overwriting default for custom value:
 impl Summary for NewsArticle {
     fn summarize_author(&self) -> String {
-        format!("@{}", self.author)
+        format!(" {}", self.author)
     }
 
     // Other methods required by summary if necessary:
     // ...
 }
 
-// Loading default:
+// Loading trait as default:
 // impl Summary for NewsArticle {}
 
 pub struct Tweet {
