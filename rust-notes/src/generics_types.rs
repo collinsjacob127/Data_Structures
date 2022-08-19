37afba47ee4931dfa60d7eefd91b3d5572dbcc66
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-18 19:13:23 -0700

    updating imports and adding notes for multiple generics

--- rust-notes/src/generics_types.rs
+++ rust-notes/src/generics_types.rs
@@ -5,15 +5,41 @@ Assessment of generics, types, and lifetimes
 */
 
 pub fn generics_types() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    mixup();
+}
 
-    let mut largest = &number_list[0];
+// Implementation of Point only for f32 type
+// required because some math operations can only be done
+// with f32
+#[allow(dead_code)]
+impl Point<f32, f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
 
-    for number in &number_list {
-        if number > largest {
-            largest = number;
+// you can change the type generic label to make operations more clear
+// You can also use different labels to notate operations where types
+// are changing throughout the operations
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
         }
     }
+}
 
-    println!("The largest number is {}", largest);
+pub fn mixup() {
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }
--- rust-notes/src/main.rs
+++ rust-notes/src/main.rs
@@ -3,13 +3,18 @@
  * Combines notes etc for output and testing
  */
 
-use collections::grouped_hash_interface;
-
 #[allow(unused_imports)]
 use crate::{
-    basic::basic, car::car, collections::collections, collections::median, collections::mode,
-    collections::pig_latin, enums::enums, error_handling::match_error_handling,
-    generics_types::generics_types, ownership::ownership,
+    basic::basic,
+    car::car,
+    collections::collections,
+    collections::median,
+    collections::mode,
+    collections::pig_latin,
+    enums::enums,
+    error_handling::match_error_handling,
+    generics_types::{generics_types, mixup},
+    ownership::ownership,
 };
 
 pub mod basic;
@@ -26,14 +31,5 @@ fn main() {
     // Rust and C both return 0 from their executables if the function processed successfully
     // A non-zero return value represents some kind of error
 
-    // basic();
-    // ownership();
-    // enums();
-    // car();
-    //     collections();
-    //    let num_list = vec![0, 1, 2, 3, 4, 4, 5];
-    //    println!("Mode: {}", mode(&num_list));
-    //    println!("Median: {}", median(&num_list));
-    //    pig_latin("A very hairy man died today. He was quite thick.");
-    grouped_hash_interface();
+    mixup();
 }
