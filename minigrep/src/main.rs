19e16f5f848a1218927ac25f8774b29553ff3f0b
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-23 22:40:39 -0700

    working command line arguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,20 @@
+/*
+Name: Jacob Collins
+Description:
+Making a program to implement a basic grep procedure in rust.
+- Command line arguments
+Last Edited: August 23 2022
+*/
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    // Use std::env::args_os if you need to collect invalid unicode characters from the command
+    // line arguments supplied
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1]; // The string we're searching for
+    let filepath = &args[2];
+
+    println!("Searching for file: {}", filepath);
+    println!("Searching for query: {}", query);
 }
