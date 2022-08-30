69556eced3e15df606fae92579cd1ba1e5ea32b5
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-30 10:32:25 -0700

    successful search and return

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -13,6 +13,10 @@ use std::{error::Error, fs};
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
