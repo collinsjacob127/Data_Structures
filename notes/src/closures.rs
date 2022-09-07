90ddca78e6e26b182d8d22bc85e60fd094664e3b
collinsjacob127 <collinsjacob127@gmail.com>
2022-09-07 13:07:27 -0700

    Basic closure variable

--- notes/src/closures.rs
+++ notes/src/closures.rs
@@ -5,4 +5,14 @@ Notes on using closures
 Last Edited: September 07 2022
 */
 
-
+pub fn closures() {
+    // Closures have a lot of optional syntax
+    //     fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+    //     let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    //     let add_one_v3 = |x|             { x + 1 };
+    //     let add_one_v4 = |x|               x + 1  ;
+    let maximum = |x| x + 1;
+    let num = 20;
+    let plus_one = maximum(num);
+    println!("{plus_one}");
+}
--- notes/src/main.rs
+++ notes/src/main.rs
@@ -3,13 +3,13 @@
  * Combines notes etc for output and testing
  */
 
-use notes::adder::add_two;
+use notes::closures::closures;
 
 fn main() {
     // Main can be allowed to return Result values, so you can use ? on functions in main()
 
     // Rust and C both return 0 from their executables if the function processed successfully
     // A non-zero return value represents some kind of error
-    let input = 3;
-    println!("{} + 2: {}", input, add_two(input));
+
+    closures();
 }
