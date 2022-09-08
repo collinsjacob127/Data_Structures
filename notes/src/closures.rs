7fade5fa239d14dd7d80e9ed3e9cfa7d16b6937d
collinsjacob127 <collinsjacob127@gmail.com>
2022-09-07 18:28:44 -0700

    Closures referencing local variables

--- notes/src/closures.rs
+++ notes/src/closures.rs
@@ -11,8 +11,16 @@ pub fn closures() {
     //     let add_one_v2 = |x: u32| -> u32 { x + 1 };
     //     let add_one_v3 = |x|             { x + 1 };
     //     let add_one_v4 = |x|               x + 1  ;
+
+    // It's like a mini function!
     let maximum = |x| x + 1;
     let num = 20;
     let plus_one = maximum(num);
     println!("{plus_one}");
+
+    let mut set = vec![1, 2, 3, 4, 5];
+    println!("Before push_10 closure: {:?}", set);
+    let mut push_10 = || set.push(10);
+    push_10();
+    println!("After push_10 closure: {:?}", set);
 }
