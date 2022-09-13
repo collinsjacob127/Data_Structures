d315bfffe8efdec368c92e36a45e02cf51d23c00
collinsjacob127 <collinsjacob127@gmail.com>
2022-09-13 10:45:20 -0700

    Progress on closures

--- notes/src/closures.rs
+++ notes/src/closures.rs
@@ -21,6 +21,37 @@ pub fn closures() {
     let mut set = vec![1, 2, 3, 4, 5];
     println!("Before push_10 closure: {:?}", set);
     let mut push_10 = || set.push(10);
+    //     println!("After push_10 closure: {:?}", set); <- this doesn't work because mutable borrow
+    //                                                      from closure definition followed by immutable
+    //                                                      borrow from print statement
     push_10();
     println!("After push_10 closure: {:?}", set);
+}
+
+/*
+*We’ll first examine how we can use closures to capture values from the environment they’re defined in for later use. Here’s the scenario: Every so often, our t-shirt company gives away an exclusive, limited-edition shirt to someone on our mailing list as a promotion. People on the mailing list can optionally add their favorite color to their profile. If the person chosen for a free shirt has their favorite color set, they get that color shirt. If the person hasn’t specified a favorite color, they get whatever color the company currently has the most of.
+
+There are many ways to implement this. For this example, we’re going to use an enum called ShirtColor that has the variants Red and Blue (limiting the number of colors available for simplicity). We represent the company’s inventory with an Inventory struct that has a field named shirts that contains a Vec<ShirtColor> representing the shirt colors currently in stock. The method giveaway defined on Inventory gets the optional shirt color preference of the free shirt winner, and returns the shirt color the person will get. This setup is shown in Listing 13-1:
+
+*/
+
+enum ShirtColor {
+    Blue,
+    Red,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_pref: Option<ShirtColor>) -> ShirtColor {
+        user_pref.unwrap_or_else(|| {
+            if self.shirts[0] < self.shirts[1] {
+                self.shirts[0]
+            } else {
+                self.shirts[1]
+            }
+        })
+    }
 }
