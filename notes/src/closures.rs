d806c1680744ff6b8795691df546409ff331ba2a
collinsjacob127 <collinsjacob127@gmail.com>
2022-09-16 12:24:17 -0700

    Changing subject of example function

--- notes/src/closures.rs
+++ notes/src/closures.rs
@@ -31,41 +31,41 @@ pub fn closures() {
 /*
 *We’ll first examine how we can use closures to capture values from the environment they’re defined in for later use. Here’s the scenario: Every so often, our t-shirt company gives away an exclusive, limited-edition shirt to someone on our mailing list as a promotion. People on the mailing list can optionally add their favorite color to their profile. If the person chosen for a free shirt has their favorite color set, they get that color shirt. If the person hasn’t specified a favorite color, they get whatever color the company currently has the most of.
 
-There are many ways to implement this. For this example, we’re going to use an enum called ShirtColor that has the variants Red and Blue (limiting the number of colors available for simplicity). We represent the company’s inventory with an Inventory struct that has a field named shirts that contains a Vec<ShirtColor> representing the shirt colors currently in stock. The method giveaway defined on Inventory gets the optional shirt color preference of the free shirt winner, and returns the shirt color the person will get. This setup is shown in Listing 13-1:
+There are many ways to implement this. For this example, we’re going to use an enum called MeatType that has the variants Mean and Lean (limiting the number of colors available for simplicity). We represent the company’s inventory with an Inventory struct that has a field named shirts that contains a Vec<MeatType> representing the shirt colors currently in stock. The method giveaway defined on Inventory gets the optional shirt color preference of the free shirt winner, and returns the shirt color the person will get. This setup is shown in Listing 13-1:
 
 */
 
 #[allow(dead_code)]
-enum ShirtColor {
-    Blue,
-    Red,
+enum MeatType {
+    Lean,
+    Mean,
 }
 
 #[allow(dead_code)]
 struct Inventory {
-    shirts: Vec<ShirtColor>,
+    shirts: Vec<MeatType>,
 }
 
 #[allow(dead_code)]
 impl Inventory {
-    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+    fn giveaway(&self, user_preference: Option<MeatType>) -> MeatType {
         user_preference.unwrap_or_else(|| self.most_stocked())
     }
 
-    fn most_stocked(&self) -> ShirtColor {
-        let mut red = 0;
-        let mut blue = 0;
+    fn most_stocked(&self) -> MeatType {
+        let mut mean = 0;
+        let mut lean = 0;
 
         for color in &self.shirts {
             match color {
-                ShirtColor::Red => red += 1,
-                ShirtColor::Blue => blue += 1,
+                MeatType::Mean => mean += 1,
+                MeatType::Lean => lean += 1,
             }
         }
-        if red > blue {
-            ShirtColor::Red
+        if mean > lean {
+            MeatType::Mean
         } else {
-            ShirtColor::Blue
+            MeatType::Lean
         }
     }
 }
